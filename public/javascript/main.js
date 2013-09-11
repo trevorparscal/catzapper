d66055a9008cad47ddcb29437d998db1597dad58
edg2s <ejsanders@gmail.com>
2013-09-11 14:19:19 -0700

    Load a random image on next button click

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,16 @@
 ( function ( $ ) {
 
+	function loadNextImage() {
+		$( '#image' ).css( 'opacity', 0.5 );
+		$.get( '/random', function ( data ) {
+			$( '#image' ).on( 'load error',  function () {
+				$( this ).css( 'opacity', '' );
+			} ).attr( 'src', data.info.thumburl );
+		} );
+	}
 
-
+	$( function () {
+		$( '#next' ).click( loadNextImage );
+	} );
 
 } ( jQuery ) );
