8822a5a9640f966e3d4465e5ccef947eebe41314
Trevor Parscal <trevorparscal@gmail.com>
2013-09-11 14:26:18 -0700

    Add categories to random image response

--- routes/random.js
+++ routes/random.js
@@ -4,23 +4,70 @@
 
 var mw = require( '../lib/mw' );
 
-function onError( error, response ) {
-	res.writeHead( 500, { 'Content-Type': 'application/json' } );
-	res.write( JSON.stringify( { 'error': error, 'response': response } ) );
-	res.end();
-}
-
 exports.random = function( req, res ) {
-	mw.api.get(
-		{
-			'action': 'query',
-			'list': 'random',
-			'rnnamespace': 6,
-			'rnlimit': 1
-		},
-		function ( data ) {
-			var image = data.query.random[0];
+	function getRandomImage( success, error ) {
+		mw.api.get(
+			{
+				'action': 'query',
+				'list': 'random',
+				'rnnamespace': 6,
+				'rnlimit': 1
+			},
+			function ( data ) {
+				success( data.query.random[0] );
+			},
+			error
+		);
+	}
 
+	getRandomImage( function ( image ) {
+		var pending = 0,
+			response = {
+				'title': image.title
+			};
+
+		function queue() {
+			var i = arguments.length;
+
+			pending += i;
+			while ( i-- ) {
+				arguments[i]();
+			}
+		}
+
+		function done() {
+			pending--;
+			if ( pending === 0 ) {
+				res.writeHead( 200, { 'Content-Type': 'application/json' } );
+				res.write( JSON.stringify( response ) );
+				res.end();
+			}
+		}
+
+		function error( error, response ) {
+			pending = -1;
+			res.writeHead( 500, { 'Content-Type': 'application/json' } );
+			res.write( JSON.stringify( { 'error': error, 'response': response } ) );
+			res.end();
+		}
+
+		function getImageCategories() {
+			mw.api.get(
+				{
+					'action': 'query',
+					'prop': 'categories',
+					'clshow': '!hidden',
+					'pageids': image.id
+				},
+				function ( data ) {
+					response.categories = data.query.pages[image.id].categories;
+					done();
+				},
+				error
+			);
+		}
+
+		function getImageInfo() {
 			mw.api.get(
 				{
 					'action': 'query',
@@ -30,15 +77,13 @@ exports.random = function( req, res ) {
 					'pageids': image.id
 				},
 				function ( data ) {
-					var info = data.query.pages[image.id].imageinfo[0];
-
-					res.writeHead( 200, { 'Content-Type': 'application/json' } );
-					res.write( JSON.stringify( { 'title': image.title, 'info': info } ) );
-					res.end();
+					response.info = data.query.pages[image.id].imageinfo[0];
+					done();
 				},
-				onError
+				error
 			);
-		},
-		onError
-	);
+		}
+
+		queue( getImageCategories, getImageInfo );
+	} );
 };
